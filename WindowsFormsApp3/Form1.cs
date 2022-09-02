0ab4949046820be3124cc1a7f1a5d7c750f0d62f
Boar_ve22@student.itstep.org <Boar_ve22@student.itstep.org>
2022-09-02 08:27:49 +0200

    upd

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -19,13 +19,13 @@ namespace WindowsFormsApp3
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.A && this.Location.X > 0)
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left && this.Location.X > 0)
                 this.Location = new Point(this.Location.X - 50, this.Location.Y);
-            else if (e.KeyCode == Keys.D && this.Location.X + this.Width < Screen.PrimaryScreen.Bounds.Width)
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right && this.Location.X + this.Width < Screen.PrimaryScreen.Bounds.Width)
                 this.Location = new Point(this.Location.X + 50, this.Location.Y);
-            else if (e.KeyCode == Keys.W && this.Location.Y > 0)
+            else if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up && this.Location.Y > 0)
                 this.Location = new Point(this.Location.X, this.Location.Y - 50);
-            else if (e.KeyCode == Keys.S && this.Location.Y + this.Height < Screen.PrimaryScreen.Bounds.Height)
+            else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down && this.Location.Y + this.Height < Screen.PrimaryScreen.Bounds.Height)
                 this.Location = new Point(this.Location.X, this.Location.Y + 50);
         }
     }
