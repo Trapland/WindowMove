44be5065285c33bef1b66a232595a3aed08a64c5
boyar <boyar@LAPTOP-GE75A9QQ>
2022-09-02 08:18:54 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && this.Location.X > 0)
+                this.Location = new Point(this.Location.X - 50, this.Location.Y);
+            else if (e.KeyCode == Keys.D && this.Location.X + this.Width < Screen.PrimaryScreen.Bounds.Width)
+                this.Location = new Point(this.Location.X + 50, this.Location.Y);
+            else if (e.KeyCode == Keys.W && this.Location.Y > 0)
+                this.Location = new Point(this.Location.X, this.Location.Y - 50);
+            else if (e.KeyCode == Keys.S && this.Location.Y + this.Height < Screen.PrimaryScreen.Bounds.Height)
+                this.Location = new Point(this.Location.X, this.Location.Y + 50);
+        }
+    }
+}
